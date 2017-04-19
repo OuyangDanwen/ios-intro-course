5b758f3497d257febf0a3dc21928aa3374d53f6b
Danwen Ouyang <ga62cer@mytum.de>
2017-04-19 15:47:46 +0200

    add solutions for S02

---
+++ Solution S02/Session 02.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+import UIKit
+
+var myName: String = "Dora Dzvonyar"
+
+func greet(name: String) {
+    print("Hello \(name)")
+}
+
+greet(name: myName)
