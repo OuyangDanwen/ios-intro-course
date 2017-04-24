bae0eceab6fb04644f9743108dbdd379a040d828
Danwen Ouyang <ga62cer@mytum.de>
2017-04-25 00:29:12 +0200

    Change typo and add missing doc for parameter “cityIndex” for SaveGuideDelegate protocol

--- Solution S05/CityGuide/SaveGuideDelegate.swift
+++ Solution S05/CityGuide/SaveGuideDelegate.swift
@@ -1,8 +1,9 @@
 protocol SaveGuideDelegate: class {
     /**
-     Save an edited "city" which replaces the original one in "cities".
+     Saves an edited "city" which replaces the original one in "cities".
      
      - Parameter cityGuide: the `city` which was edited and is to replace the original one
+     - Parameter cityIndex: the index at vhich the "city" can be located in "cities"
      
      */    func saveGuide(cityGuide: City, cityIndex: Int)
 }
