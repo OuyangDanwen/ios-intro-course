19b050395a06900a492e7fa0fe3fbdd7daac39e3
Danwen Ouyang <ga62cer@mytum.de>
2017-04-25 21:08:23 +0200

    complete solution to ex9

--- Solution S09/SwiftMail/SwiftMail/Presentation/SplashScreen/LaunchScreenViewController.swift
+++ Solution S09/SwiftMail/SwiftMail/Presentation/SplashScreen/LaunchScreenViewController.swift
@@ -13,6 +13,7 @@ class LaunchScreenViewController: UIViewController {
         
         animate()
         
+        //delay the segue for the segue to complete
         let duration = DispatchTime.now() + 3
         DispatchQueue.main.asyncAfter(deadline: duration) {
             self.performSegue(withIdentifier: self.segueIdentifierForViewMailVC, sender: nil)
@@ -23,7 +24,7 @@ class LaunchScreenViewController: UIViewController {
     private func animate() {
         UIView.animate(withDuration: 1.5, animations: {
             self.iconImage.transform = CGAffineTransform.init(rotationAngle: 90) }){ (true) in UIView.animate(withDuration: 1.5, animations: {
-                self.iconImage.transform = CGAffineTransform.init(translationX: 0, y: -100)}, completion: { (true) in })
+                self.iconImage.transform = CGAffineTransform.init(translationX: 0, y: -200)}, completion: { (true) in })
         }
     }
 }
